9667008e79aff5529ccdead5068204cf04efb125
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2023-02-26 11:51:06 +0100

    Add items

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Inventory
 {
     public class Item : MonoBehaviour
     {
+        private Image image;
 
+        public bool HasItem { get; private set; } = false;
+
+        private void Awake()
+        {
+            if (TryGetComponent(out image))
+            {
+                HasItem = image.sprite != null;
+                if (!HasItem)
+                    image.color = Color.clear;
+            }
+        }
     }
 }
