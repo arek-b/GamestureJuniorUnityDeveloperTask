7c957baff7b058f2064630e09abf84c5997a750b
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2023-02-28 11:06:14 +0100

    Optimize performabce & disable adaprive performance

---
+++ Assets/Scripts/Performance/PerformanceManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Performance
+{
+    public class PerformanceManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Application.targetFrameRate = 60;
+        }
+    }
+}
